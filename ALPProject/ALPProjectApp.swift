887e37bd2ac40b9301a452feaacaf42786f9501e
Evan Tanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2024-05-10 08:12:59 +0700

    Initial Commit

---
+++ ALPProject/ALPProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALPProjectApp.swift
+//  ALPProject
+//
+//  Created by Evan Tanuwijaya on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ALPProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
