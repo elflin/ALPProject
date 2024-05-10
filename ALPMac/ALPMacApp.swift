4e085aed902d2220ad9d49511434c142e5fc8f02
Evan Tanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2024-05-10 08:28:06 +0700

    First Commit

---
+++ ALPMac/ALPMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALPMacApp.swift
+//  ALPMac
+//
+//  Created by Evan Tanuwijaya on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ALPMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALPWatch Watch App/ALPWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALPWatchApp.swift
+//  ALPWatch Watch App
+//
+//  Created by Evan Tanuwijaya on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ALPWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
